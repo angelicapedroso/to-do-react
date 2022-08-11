2e9ecf74334ec886110080d14306086c078855e8
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-11 10:02:02 -0300

    css home page

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 10px;
+  color: var(--color-five);
+`;
+
+export const Header = styled.h1`
+  color: var(--color-five);
+  text-align: center;
+  border-bottom: 1px solid var(--color-two);
+  padding-bottom: 20px;
+`;
