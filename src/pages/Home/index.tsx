310a647378bdc231c22680423aa01ade2a415c6a
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-11 10:00:13 -0300

    feat: content and css home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { Content, Header } from './style';
+import React, { useState } from 'react';
+import List from '../../components/List';
+import { Item } from '../../types/Item';
+import { Content, Header } from './styles';
 
 export default function Home(): React.FunctionComponentElement<{}> {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Item 1', done: false },
+    { id: 2, name: 'Item 1', done: false },
+  ]);
+
   return (
     <Content>
       <Header>Lista de Tarefas</Header>
+      {
+        list.map((item) => (
+          <List key={item.id} item={item} />
+        ))
+      }
     </Content>
   );
 }
