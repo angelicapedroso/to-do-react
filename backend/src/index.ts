0631ad490997928b9b3373a82b71954fdc633e8c
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-18 23:43:57 -0300

    refactor: change port backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
